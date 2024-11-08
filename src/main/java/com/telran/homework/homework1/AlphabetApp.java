c3cf8e21d970dd17cc7ce02443616189b13dd7ba
Serhii Kurlykin <kurlykin83@gmail.com>
2024-11-08 09:56:54 +0100

    add homework1

---
+++ src/main/java/com/telran/homework/homework1/AlphabetApp.java
@@ -0,0 +1,27 @@
+package org.telran.homework.homework1;
+
+public class AlphabetApp {
+    public static void main(String[] args) {
+
+        UkraineAlphabet ukraineAlphabet = new UkraineAlphabet();
+        EnglishAlphabet englishAlphabet = new EnglishAlphabet();
+        GermanAlphabet germanAlphabet = new GermanAlphabet();
+
+
+        ukraineAlphabet.printAlphabet();
+        englishAlphabet.printAlphabet();
+        germanAlphabet.printAlphabet();
+
+
+        Alphabet[] alphabets = {ukraineAlphabet, englishAlphabet, germanAlphabet};
+        printAllInfo(alphabets);
+    }
+
+    public static void printAllInfo(Alphabet[] alphabets) {
+        for (Alphabet alphabet : alphabets) {
+            alphabet.printAlphabet();
+        }
+    }
+
+}
+
