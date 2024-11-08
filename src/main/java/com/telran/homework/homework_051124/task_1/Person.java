8238da016eec562a1e73b2668a430748191786f5
Ant0nSB <Jackpot.8.8.8.8.8A@gmail.com>
2024-11-08 10:21:55 +0100

    homework051124

---
+++ src/main/java/com/telran/homework/homework_051124/task_1/Person.java
@@ -0,0 +1,36 @@
+package com.telran.homework.homework_051124.task_1;
+
+public class Person {
+    private String name;
+    private String fullName;
+    private  int age;
+
+    public Person() {
+    }
+
+    public Person(String name, String fullName, int age) {
+        this.name = name;
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public void move(){
+        System.out.println(fullName +" идёт пешком.");
+    }
+
+    public void talk(){
+        System.out.println(fullName + " говорит Hello!");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
