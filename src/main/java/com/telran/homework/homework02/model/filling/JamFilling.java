52981b81156ae12e9a8e6c4690e17a1d22429d9c
Roman Dogoda <piterom911@gmail.com>
2024-11-12 23:06:54 +0100

    implement homework 2 —> Donuts

---
+++ src/main/java/com/telran/homework/homework02/model/filling/JamFilling.java
@@ -0,0 +1,8 @@
+package com.telran.homework.homework02.model.filling;
+
+public class JamFilling extends Filling {
+
+    public JamFilling(String title) {
+        super(title);
+    }
+}
---
+++ src/main/java/com/telran/homework/homework02/model/topping/ChocolateTopping.java
@@ -0,0 +1,8 @@
+package com.telran.homework.homework02.model.topping;
+
+public class ChocolateTopping extends Topping {
+
+    public ChocolateTopping(String title) {
+        super(title);
+    }
+}
