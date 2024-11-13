93bb509c5096b61b75bb4b0f67d5dfd77c32cdef
Julia Frolova <jmabracodabra@gmail.com>
2024-11-14 00:20:53 +0100

    Added Donut Machine

---
+++ src/main/java/com/telran/homework2/donut/model/DonutWithStrawberry.java
@@ -0,0 +1,8 @@
+package com.telran.homework2.donut.model;
+
+public class DonutWithStrawberry extends DonutWithFilling {
+
+    public DonutWithStrawberry(String title) {
+        super(title);
+    }
+}
