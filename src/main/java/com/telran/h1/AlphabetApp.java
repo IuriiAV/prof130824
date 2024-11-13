5d4f874281917775e0444fd6273ee6d14df591f0
Kostiantyn19899 <sierikov.de@gmail.com>
2024-11-13 12:33:07 +0100

    add homework 1 (#3)
    Co-authored-by: Kostiantyn19899 <sierikov.de@gmail.cim>

---
+++ src/main/java/com/telran/h1/AlphabetApp.java
@@ -0,0 +1,27 @@
+package com.telran.h1;
+
+public class AlphabetApp {
+    public static void main(String[] args) {
+
+        AlphabetEng alphabetEng = new AlphabetEng("English");
+        alphabetEng.setName("English");
+        alphabetEng.setOwnNumber(1);
+        System.out.println(alphabetEng.getOwnNumber());
+        alphabetEng.printAlphabet();
+
+        AlphabetDeu alphabetDeu = new AlphabetDeu("Deutsch");
+        alphabetDeu.setOwnNumber(2);
+        System.out.println(alphabetDeu.getOwnNumber());
+        alphabetDeu.printAlphabet();
+
+        AlphabetRus alphabetRus = new AlphabetRus("Russia");
+        alphabetRus.setOwnNumber(3);
+        System.out.println(alphabetRus.getOwnNumber());
+        alphabetRus.printAlphabet();
+
+        Alphabet all = new Alphabet("All");
+        all.printAllInfo(alphabetEng,alphabetDeu,alphabetRus);
+
+
+    }
+}
