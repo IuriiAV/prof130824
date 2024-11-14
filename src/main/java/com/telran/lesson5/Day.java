d33d608e354a556d45a5bb31c78934adcf3a62f7
IuriiAv <avramenko.ca@gmail.com>
2024-11-14 13:06:35 +0300

    add lesson 5 part one

---
+++ src/main/java/com/telran/lesson5/Day.java
@@ -0,0 +1,15 @@
+package com.telran.lesson5;
+
+//java.lang.Enum
+public enum Day {
+
+    // Day MONDAY = new Day("MONDAY");
+    // public static final
+    MONDAY,  // 0
+    TUESDAY, // 1
+    WEDNESDAY, // 2
+    THURSDAY, // 3
+    FRIDAY,  // 4
+    SATURDAY, //5
+    SUNDAY // 6
+}
