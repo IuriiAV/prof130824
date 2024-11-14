84b03e4539e13d6b1b70b2e0b6a34ee0ad0c4a71
Alexandra_Kriviz <kriviz.alexandra@gmail.com>
2024-11-14 23:14:40 +0100

    Add gitIgnore file

--- src/main/java/com/telran/lesson5/deal/add/Deck.java
+++ src/main/java/com/telran/lesson5/deal/add/Deck.java
@@ -1,15 +1,37 @@
 package com.telran.lesson5.deal.add;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
 
     private Card[] cards = new Card[Rank.values().length * Suit.values().length];
 
     public void fillCards() {
+        Rank[] values = Rank.values();
+        int counter = 0; //индекс в массиве
+        for (int i = 0; i < values.length; i++) {
+            cards[counter++] = new Card(Suit.CLUBS, values[i]);
+            cards[counter++] = new Card(Suit.HEARTS, values[i]);
+            cards[counter++] = new Card(Suit.DIAMONDS, values[i]);
+            cards[counter++] = new Card(Suit.SPADES, values[i]);
+        }
+        shuffle();
 
-
+    }
+    public void shuffle() {
+        List<Card> list = Arrays.asList(this.cards);
+        Collections.shuffle(list);
+        for (int i = 0; i < list.size(); i++) {
+            cards[i] = list.get(i);
+        }
     }
 
     public Card getCard() {
-        return null;
+
+        Card result = cards [cards.length - 1];
+        cards = Arrays.copyOf(cards, cards.length -1);
+        return result;
     }
 }
--- src/main/java/com/telran/lesson5/deal/add/Player.java
+++ src/main/java/com/telran/lesson5/deal/add/Player.java
@@ -1,24 +1,31 @@
 package com.telran.lesson5.deal.add;
 
 
+import java.util.Arrays;
+
 public class Player {
 
     private String name;
+
+    private Card[] cards = new Card[5];
+    private int index = 0;
 
     public Player(String name) {
         this.name = name;
     }
 
     public void takeCard(Card card) {
+        cards[index++] = card;
 
     }
 
     public void printCards() {
+        System.out.println(Arrays.deepToString(cards));
 
     }
 
     public void shuffleDeck(Deck deck) {
-
+        deck.shuffle();
     }
 
     public String getName() {
