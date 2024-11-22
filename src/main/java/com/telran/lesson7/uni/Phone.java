b2fdd2670ed7a40fd0e29b2063b26029e6c60d88
Kostiantyn19899 <sierikov.de@gmail.cim>
2024-11-22 09:34:53 +0100

    add lesson7

--- src/main/java/com/telran/lesson7/uni/Phone.java
+++ src/main/java/com/telran/lesson7/uni/Phone.java
@@ -1,8 +1,38 @@
 package com.telran.lesson7.uni;
 
+/**
+ * get info about students , get list of phone numbers
+ * cell operator code, number
+ * get info about code and number
+ * get full number(code+number)
+ */
 public class Phone {
 
-    int codOperator;
+    String codOperator;
 
-    int number;
+    String number;
+
+    public Phone(String codOperator, String number) {
+        this.codOperator = codOperator;
+        this.number = number;
+    }
+
+
+    public String getFullNumber(){
+        return getCodOperator()+getNumber();
+    }
+
+
+    public String getCodOperator() {
+        return codOperator;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone " + getFullNumber() ;
+    }
 }
