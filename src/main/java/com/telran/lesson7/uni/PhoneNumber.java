5dd7a42587579137f8d24b99a4e1b1d7532bc835
Roman Dogoda <piterom911@gmail.com>
2024-11-25 20:58:12 +0100

    hw5 University completed

--- src/main/java/com/telran/lesson7/uni/PhoneNumber.java
+++ src/main/java/com/telran/lesson7/uni/PhoneNumber.java
@@ -2,8 +2,44 @@ package com.telran.lesson7.uni;
 
 public class PhoneNumber {
 
-    // cell operator code, number
+    private String code;
+    private String number;
 
-    // get info about code and number
-    // get full number(code+number)
+    public PhoneNumber(String code, String number) {
+        this.code = checkCode(code);
+        this.number = checkNumber(number);
+    }
+
+    private String checkCode(String code) {
+        String operatorCodeRegex = "\\d{3}";
+        if (!code.matches(operatorCodeRegex)) {
+            throw new IllegalArgumentException("Wrong code: " + code);
+        }
+        return code;
+    }
+
+    private String checkNumber(String number) {
+        String phoneNumberRegex = "\\d{3}-\\d{3}-\\d{2}";
+        if (!number.matches(phoneNumberRegex)) {
+            throw new IllegalArgumentException("Wrong number format: " + number);
+        }
+        return number;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getNumber() {
+        return "0" + number;
+    }
+
+    public String getFullNumber() {
+        return code + number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone number: +49" + code + number;
+    }
 }
