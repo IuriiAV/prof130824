b8caca0e4692515783946de7faf4e5271a2dd2b8
Serhii Kurlykin <kurlykin83@gmail.com>
2024-11-28 15:05:59 +0100

    //to HW : get(index) , remove(index), add(index, data)         //removeFirst(), removeLast() , size()         //* Add iterator, add second direction

---
+++ src/main/java/com/telran/lesson8/custommine/CustomIterator.java
@@ -0,0 +1,27 @@
+package com.telran.lesson8.custommine;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomIterator implements Iterator<Integer> {
+    private Node current;
+
+    public CustomIterator(Node head) {
+        this.current = head;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int data = current.getData();
+        current = current.getNext();
+        return data;
+    }
+}
