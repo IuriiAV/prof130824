9c11ebdbec3da3c949205eb4e10aaba39837ea83
Julia Frolova <jmabracodabra@gmail.com>
2024-12-02 22:05:00 +0100

    Added Homework 5 (Uni)

--- src/main/java/com/telran/myLesson7/uni/PhoneNumber.java
+++ src/main/java/com/telran/myLesson7/uni/PhoneNumber.java
@@ -3,6 +3,32 @@ package com.telran.myLesson7.uni;
 public class PhoneNumber {
     //cell operator code, number
 
-//    get info about code and number
+    //get info about code and number
     //get full number (code + number)
+
+    private int number;
+
+    private int code;
+
+    public PhoneNumber(int code, int number) {
+        this.number = number;
+        this.code = code;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getFullNumber() {
+        return code + " " + number;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + number;
+    }
 }
--- src/main/java/com/telran/myLesson7/uni/University.java
+++ src/main/java/com/telran/myLesson7/uni/University.java
@@ -1,5 +1,9 @@
 package com.telran.myLesson7.uni;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class University {
     //variables:
     //list of students
@@ -10,4 +14,72 @@ public class University {
     //add a student in uni, and check that there's no student with the same name and age
     //remove students from uni and add him to black list
     //up rate for all students (+1)
+
+    private final List<Student> students = new ArrayList<>();
+
+    private final List<Student> blackList = new ArrayList<>();
+
+    public University() {
+        this(
+                List.of(
+                        new Student("Peter", 21),
+                        new Student("Nana", 19),
+                        new Student("Juju", 22),
+                        new Student("Max", 19),
+                        new Student("Claus", 21),
+                        new Student("Mimi", 21),
+                        new Student("Alex", 19),
+                        new Student("TJ", 22),
+                        new Student("Sara", 19),
+                        new Student("Petra", 21)
+                )
+        );
+    }
+
+    public University(List<Student> students) {
+        if (students != null) {
+            this.students.addAll(students);
+        }
+    }
+
+    public void addStudent(Student student) {
+        if (students.contains(student)) {
+            System.out.println("Such a student is already studying at the university.");
+        } else {
+            students.add(student);
+        }
+    }
+
+    public void addToBlackListStudent(String name, int age) {
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            Student student = iterator.next();
+            if (student.getName().equals(name) && student.getAge() == age) {
+                this.blackList.add(student);
+                iterator.remove();
+            }
+        }
+    }
+
+    public void upRateForStudents() {
+        for (Student student : students) {
+            student.setRate(student.getRate() + 1);
+        }
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public List<Student> getBlackList() {
+        return blackList;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "students=" + students +
+                ", blackList=" + blackList +
+                '}';
+    }
 }
