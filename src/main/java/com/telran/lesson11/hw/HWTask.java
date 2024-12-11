7bf6327d049f7ea8921a9e2f1c6c811951545a9c
Julia Frolova <jmabracodabra@gmail.com>
2024-12-11 09:50:02 +0100

    Added Patient class

--- src/main/java/com/telran/lesson11/hw/HWTask.java
+++ src/main/java/com/telran/lesson11/hw/HWTask.java
@@ -1,12 +1,19 @@
 package com.telran.lesson11.hw;
 
-import java.util.Deque;
 import java.util.PriorityQueue;
 
 public class HWTask {
 
     public static void main(String[] args) {
         PriorityQueue<Patient> objects = new PriorityQueue<>(10);
+
+        objects.add(new Patient("Babushka Silver", 65, 3));
+        objects.add(new Patient("Babushka Gold", 65, 2));
+        objects.add(new Patient("Lola", 22, 2));
+
+        for (Patient patient : objects) {
+            System.out.println(patient);
+        }
 
     }
 }
--- src/main/java/com/telran/lesson11/hw/Patient.java
+++ src/main/java/com/telran/lesson11/hw/Patient.java
@@ -1,6 +1,42 @@
 package com.telran.lesson11.hw;
 
-public class Patient {
+public class Patient implements Comparable<Patient> {
 
+    private String name;
 
+    private int age;
+
+    private int status;
+
+    public Patient(String name, int age, int status) {
+        this.name = name;
+        this.age = age;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    @Override
+    public int compareTo(Patient o) {
+        int result = o.age - this.age;
+        return result == 0 ? this.status - o.status : result;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                        ", age=" + age +
+                        ", status=" + status +
+                        '\n';
+    }
 }
