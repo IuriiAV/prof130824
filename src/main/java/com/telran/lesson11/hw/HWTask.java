a4f6270bc1b51e7b538528692c524503d477a98d
Julia Frolova <jmabracodabra@gmail.com>
2024-12-12 23:32:17 +0100

    Changed printing of PriorityQueue

--- src/main/java/com/telran/lesson11/hw/HWTask.java
+++ src/main/java/com/telran/lesson11/hw/HWTask.java
@@ -7,13 +7,12 @@ public class HWTask {
     public static void main(String[] args) {
         PriorityQueue<Patient> objects = new PriorityQueue<>(10);
 
+        objects.add(new Patient("Lola", 22, 2));
         objects.add(new Patient("Babushka Silver", 65, 3));
         objects.add(new Patient("Babushka Gold", 65, 2));
-        objects.add(new Patient("Lola", 22, 2));
 
-        for (Patient patient : objects) {
-            System.out.println(patient);
+        while (!objects.isEmpty()){
+            System.out.println(objects.poll());
         }
-
     }
 }
