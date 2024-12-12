d2b53fa40b671a9f09356a0163e9f0eb077eb700
IuriiAv <avramenko.ca@gmail.com>
2024-12-12 09:05:28 +0300

    add lesson 12

---
+++ src/main/java/com/telran/lesson12/CarExample.java
@@ -0,0 +1,9 @@
+package com.telran.lesson12;
+
+public class CarExample {
+
+    public static void main(String[] args) {
+        //Car.Engine engine = new Car.Engine();
+        Car.Engine engine1 = new Car().new Engine();
+    }
+}
