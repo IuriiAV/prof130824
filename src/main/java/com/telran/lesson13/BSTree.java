ced58bdc9e8044f4376fefac596267158f3be3af
IuriiAv <avramenko.ca@gmail.com>
2024-12-12 14:48:17 +0300

    add lesson 13

---
+++ src/main/java/com/telran/lesson13/BSTree.java
@@ -0,0 +1,16 @@
+package com.telran.lesson13;
+
+/**
+ *               14
+ *             /   \
+ *          6        18
+ *        /  \      /  \
+ *       5   7     16   22
+ *     /      \   /    /  \
+ *    4        8 15   20   24
+ *
+ *     7 ->  8 -> 7
+ */
+
+public class BSTree {
+}
