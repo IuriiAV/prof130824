9fd2bd5e87fc8692f89dde7ce1401d997a4a93bf
Roman Dogoda <piterom911@gmail.com>
2024-12-16 16:04:01 +0100

    add cycle till getting win

--- src/main/java/com/telran/homework/homework8/solitaire/SolitaireApp.java
+++ src/main/java/com/telran/homework/homework8/solitaire/SolitaireApp.java
@@ -1,5 +1,6 @@
 package com.telran.homework.homework8.solitaire;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -10,7 +11,17 @@ public class SolitaireApp {
         List<Card> cards = deck.getCards();
         System.out.println(cards);
 
-        System.out.println(playGame(cards));
+        int attempt = 0;
+        while (true) {
+            attempt++;
+            Stack<Card> result = playGame(cards);
+            if (result.size() == 4) {
+                System.out.println("Number of attempts to win: " + attempt);
+                System.out.println(result);
+                break;
+            }
+            Collections.shuffle(cards);
+        }
     }
 
     private static Stack<Card> playGame(List<Card> cards) {
