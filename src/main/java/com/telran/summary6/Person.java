bc3641c92f61c70b0c11579f0e2fba9e3bbfce5c
Kostiantyn19899 <sierikov.de@gmail.cim>
2024-12-20 12:44:31 +0100

    add summary 6

---
+++ src/main/java/com/telran/summary6/Person.java
@@ -0,0 +1,19 @@
+package com.telran.summary6;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Person {
+
+    private String name;
+
+    private String suName;
+
+    private int id;
+
+    private int rate;
+}
