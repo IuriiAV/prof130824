85d138a5c93d4fccca3a036668de878acbfadb40
Maria <shirokova.maria14@gmail.com>
2025-01-22 14:48:52 +0100

    add hw7

--- src/main/java/com/telran/lesson11/hw/HWTask.java
+++ src/main/java/com/telran/lesson11/hw/HWTask.java
@@ -6,7 +6,19 @@ import java.util.PriorityQueue;
 public class HWTask {
 
     public static void main(String[] args) {
-        PriorityQueue<Patient> objects = new PriorityQueue<>(10);
+        PriorityQueue<Patient> objects = new PriorityQueue<>(10, new PatientComparator());
+        objects.add(new Patient("Anna", 25, true));
+        objects.add(new Patient("Alona", 29, true));
+        objects.add(new Patient("Oleg", 28, false));
+        objects.add(new Patient("Peter", 45, true));
+        objects.add(new Patient("Anastasia", 67, false));
+        objects.add(new Patient("Irina", 4, false));
+        objects.add(new Patient("Maria", 25, true));
+        objects.add(new Patient("Inna", 26, false));
+        objects.add(new Patient("Alex", 20, false));
+        objects.add(new Patient("Nadia", 57, true));
 
+        System.out.println(objects);
     }
+
 }
--- src/main/java/com/telran/lesson11/hw/Patient.java
+++ src/main/java/com/telran/lesson11/hw/Patient.java
@@ -1,6 +1,62 @@
 package com.telran.lesson11.hw;
 
+import java.util.Objects;
+
 public class Patient {
+    private String name;
+    private int age;
 
+    private boolean isUrgent;
 
+    public Patient(String name, int age, boolean iUrgent) {
+        this.name = name;
+        this.age = age;
+        this.isUrgent= iUrgent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isUrgent() {
+        return isUrgent;
+    }
+
+    public void setUrgent(boolean iUrgent) {
+        this.isUrgent = iUrgent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Patient patient = (Patient) o;
+        return age == patient.age && isUrgent == patient.isUrgent && Objects.equals(name, patient.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, isUrgent);
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", isUrgent=" + isUrgent +
+                '}';
+    }
 }
