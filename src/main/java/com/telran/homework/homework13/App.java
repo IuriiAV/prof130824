5910cac574a95bc6492d5e92186d0f83524d40c1
Roman Dogoda <piterom911@gmail.com>
2025-01-23 11:07:18 +0100

    summary 13 Dictionary lesson part

---
+++ src/main/java/com/telran/homework/homework13/App.java
@@ -0,0 +1,14 @@
+package com.telran.homework.homework13;
+
+import com.telran.summary.summary19.translator.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        TranslatorFileWriter writer = new TranslatorFileWriter();
+        TranslatorFileReader reader = new TranslatorFileReader();
+        Dictionary dictionary = new Dictionary(writer, reader);
+
+        new DictionaryController(dictionary);
+    }
+}
